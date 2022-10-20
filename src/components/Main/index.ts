08795dfe4f44844286997fe725fd5c852a922cbf
Mira <6984271+szewczykmira@users.noreply.github.com>
2022-10-20 21:00:54 +0200

    Raname files to index.ts

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -2,8 +2,9 @@ import { LitElement, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import { styles } from './styles.js';
 
-import '../Footer/Footer.js';
-import '../Header/Header.js';
+import '../Footer/index.js';
+import '../Header/index.js';
+import '../ListOfCharacters/index.js';
 
 export class DinaApp extends LitElement {
   @property({ type: String }) title = 'My fancy app';
@@ -19,7 +20,9 @@ export class DinaApp extends LitElement {
           title=${this.title}
           helpUrl="https://open-wc.org/guides/developing-components/code-examples"
         ></header-container>
-        <div>${this.objects.map(item => html`<div>${item}</div>`)}</div>
+        <list-of-characters-component
+          .characters=${this.objects}
+        ></list-of-characters-component>
       </main>
       <footer-container url="https://github.com/open-wc"></footer-container>
     `;
