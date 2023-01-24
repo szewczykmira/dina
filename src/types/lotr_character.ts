492d0be3fd8726a88c1d1fc5af9ae67b8857942f
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-01-24 22:28:11 +0100

    Mark fields that are missing as optional

--- src/types/lotr_character.ts
+++ src/types/lotr_character.ts
@@ -2,12 +2,12 @@ export default interface LotrCharacter {
   _id: string;
   height: string;
   race: string; // can I do specific value?
-  gender: string;
+  gender?: string;
   birth: string;
   spouse: string;
   death: string;
   realm: string;
   hair: string;
   name: string;
-  wikiUrl: string;
+  wikiUrl?: string;
 }
