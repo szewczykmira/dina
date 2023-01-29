1c75fc10665e94ad50c6ca561a8027cf144d8ff7
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-01-29 22:10:47 +0100

    Use header tag inside header component

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,13 +1,18 @@
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
+import { styles } from './styles.js';
+import { colors } from '../../../static/shared.js';
+
 @customElement('header-container')
 export class HeaderElement extends LitElement {
   @property({ type: String }) title = '';
 
+  static styles = [colors, styles];
+
   render() {
-    return html`<div>
+    return html`<header>
       <h1>${this.title}</h1>
-    </div>`;
+    </header>`;
   }
 }
