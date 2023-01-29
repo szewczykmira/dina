f8285b254fade62ed441c5e683eeb1571f56cb30
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-01-29 22:38:40 +0100

    Set headers background and margin

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import { css } from 'lit';
+
+export const styles = css`
+  header {
+    margin: 0 0 auto 0;
+    background-color: var(--jet);
+    color: var(--paradise-pink);
+    padding: 2em;
+    text-align: center;
+  }
+`;
