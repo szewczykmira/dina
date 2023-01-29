74cfade2e94e7ac4510f9fdfdfa24b5700e60585
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-01-29 22:41:18 +0100

    Style text

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,9 +3,12 @@ import { css } from 'lit';
 export const styles = css`
   header {
     margin: 0 0 auto 0;
+    padding: 2em;
     background-color: var(--jet);
     color: var(--paradise-pink);
-    padding: 2em;
+    /*text*/
     text-align: justify;
+    font-style: italic;
+    font-variant: small-caps;
   }
 `;
