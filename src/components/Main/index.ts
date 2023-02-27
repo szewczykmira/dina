8d13baa960502a04c19df171a3f8c87a9dbbd7f5
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-02-27 10:19:47 +0100

    Add buttons to scroll up and down. They are font awesome icons

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -2,6 +2,7 @@ import { LitElement, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import { styles } from './styles.js';
 import { colors } from '../../../static/shared.js';
+import 'fa-icons';
 
 import '../Footer/index.js';
 import '../Header/index.js';
@@ -17,13 +18,47 @@ export class DinaApp extends LitElement {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
+  goDown = () => {
+    document.body.scrollTop = document.body.scrollHeight; // For Safari
+    document.documentElement.scrollTop = document.body.scrollHeight; // For Chrome, Firefox, IE and Opera
+  };
+
+  _handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case 'd': {
+        this.goDown();
+        break;
+      }
+      case 'u': {
+        this.goUp();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  // TODO: make sure that footer sticks to bottom
+
   render() {
     return html`
       <header-container title=${this.title}></header-container>
       <main>
         <list-of-characters-component></list-of-characters-component>
-        <div class="go-up-button">
-          <button @click=${this.goUp}>Yep Yep</button>
+        <div
+          class="go-up-button change-location"
+          @click=${this.goUp}
+          @keyDown=${this._handleKeyDown}
+        >
+          <fa-icon class="fas fa-arrow-up"></fa-icon>
+        </div>
+        <div
+          class="go-down-button change-location"
+          @click=${this.goDown}
+          @keyDown=${this._handleKeyDown}
+        >
+          <fa-icon class="fas fa-arrow-down"></fa-icon>
         </div>
       </main>
       <footer-container
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -11,9 +11,25 @@ export const styles = css`
     flex-grow: 1;
   }
 
-  .go-up-button {
+  .change-location {
     position: fixed;
+    background-color: var(--teal);
+    z-index: 10000;
+    padding: 0.3em;
+  }
+  .change-location:hover {
+    cursor: pointer;
+  }
+  .go-up-button {
     bottom: 5%;
     right: 5%;
+    margin-right: 1%;
+    border-radius: 5px 25px 15px;
+  }
+
+  .go-down-button {
+    bottom: 5%;
+    right: 3%;
+    border-radius: 15px 25px 5px;
   }
 `;
