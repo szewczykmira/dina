14a2d149783f213ac0091ccf50db4c038d906810
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-03-16 23:11:42 +0100

    Define race type

--- src/types/lotr_character.ts
+++ src/types/lotr_character.ts
@@ -3,11 +3,50 @@ type Female = 'Female';
 type UndefinedGender = '' | 'NaN';
 type Gender = Female | Male | UndefinedGender;
 
+type UnknownRace = 'NaN' | '';
+type GodLike = 'Maiar' | 'Ainur' | 'God';
+type Elf = 'Elf' | 'Elves' | 'Half-elven';
+type Human = 'Human' | 'Men';
+type Hobbit = 'Hobbit' | 'Hobbits';
+type Ent = 'Ent' | 'Ents';
+type Dwarf = 'Dwarf' | 'Dwarves';
+type Dragon = 'Dragon' | 'Dragons';
+type Eagle = 'Eagles' | 'Great Eagles' | 'Eagle';
+type Animals = 'Great Spiders' | 'Raven' | 'Horse' | Eagle;
+type Orc =
+  | 'Orcs'
+  | 'Black Uruk'
+  | 'Uruk-hai'
+  | 'Orc'
+  | 'Goblin,Orc'
+  | 'Orc,Goblin'
+  | 'Uruk-hai,Orc';
+type EvilRaces =
+  | Orc
+  | 'Werewolves'
+  | 'Men,Wraith'
+  | 'Balrog'
+  | 'Urulóki'
+  | 'Stone-trolls'
+  | 'Maiar,Balrogs';
+type Race =
+  | Human
+  | Elf
+  | Dwarf
+  | Hobbit
+  | GodLike
+  | Ent
+  | 'Wolfhound'
+  | EvilRaces
+  | Dragon
+  | UnknownRace
+  | Animals;
+
 export default interface LotrCharacter {
   _id: string;
   height: string;
-  race: string;
-  gender: Gender;
+  race: Race;
+  gender?: Gender;
   birth: string;
   spouse: string;
   death: string;
