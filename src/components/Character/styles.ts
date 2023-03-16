96879f8b8ac8c9ba2d23d93d7e5e8751bb8aa0ba
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-03-16 22:33:50 +0100

    Guard characters per line using @media

--- src/components/Character/styles.ts
+++ src/components/Character/styles.ts
@@ -1,6 +1,22 @@
 import { css } from 'lit';
 
 export const styles = css`
+  :host {
+    width: 33%;
+  }
+
+  @media (max-width: 800px) {
+    :host {
+      width: 100%;
+    }
+  }
+
+  @media (min-width: 801px) and (max-width: 1000px) {
+    :host {
+      width: 50%;
+    }
+  }
+
   .characterCard {
     border: solid;
     margin: 20px;
