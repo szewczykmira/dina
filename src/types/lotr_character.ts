2b14b217eb2b623bc9bb3f9eaf91bb381427b8db
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-03-16 22:56:05 +0100

    Define gender type

--- src/types/lotr_character.ts
+++ src/types/lotr_character.ts
@@ -1,8 +1,13 @@
+type Male = 'Male' | 'Most likely male' | 'Males' | 'male';
+type Female = 'Female';
+type UndefinedGender = '' | 'NaN';
+type Gender = Female | Male | UndefinedGender;
+
 export default interface LotrCharacter {
   _id: string;
   height: string;
-  race: string; // can I do specific value?
-  gender?: string;
+  race: string;
+  gender: Gender;
   birth: string;
   spouse: string;
   death: string;
