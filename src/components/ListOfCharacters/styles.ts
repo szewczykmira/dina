44724b25991fb4f215ccdf9f66f5c49259306b2c
Mira <6984271+szewczykmira@users.noreply.github.com>
2023-06-11 00:00:21 +0200

    Style buttons

--- src/components/ListOfCharacters/styles.ts
+++ src/components/ListOfCharacters/styles.ts
@@ -7,4 +7,18 @@ export const styles = css`
     flex-wrap: wrap;
     justify-content: space-evenly;
   }
+  button {
+    margin-left: 1rem;
+    background-color: var(--teal);
+    border: none;
+    color: var(--grey);
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+  button:disabled {
+    color: var(--teal);
+  }
 `;
